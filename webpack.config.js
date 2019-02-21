3afd7f31cba07d8d00f85e2fe8a6f477366599af
Paul <paul@paulgoesdeep.com>
2019-02-21 23:28:53 +0300

    Basic project structure with webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+const CopyPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/js/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CopyPlugin([
+      { from: 'src/index.html'},
+      { from: 'src/css', to: 'css/'},
+      { from: 'static'}
+    ]),
+  ],
+};
