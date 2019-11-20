81d484a61e0a896226cc06e1a9ba06668623a57a
Paul <paul@paulgoesdeep.com>
2019-11-20 10:43:30 +0300

    Change move highlight color

--- src/js/game/overlay.ts
+++ src/js/game/overlay.ts
@@ -4,8 +4,8 @@ import { Board } from "../board";
 class Overlay {
   private board: Board;
 
-  private readonly MOVE_HIGHLIGHT_COLOR = "rgba(0, 102, 204, 0.5)";
-  private readonly ENEMY_HIGHLIGHT_COLOR = "rgba(255, 0, 51, 0.5)";
+  private readonly MOVE_HIGHLIGHT_COLOR = "rgba(102, 204, 0, 0.5)";
+  private readonly TARGET_HIGHLIGHT_COLOR = "rgba(255, 0, 51, 0.5)";
 
   constructor(board: Board) {
     this.board = board;
@@ -16,7 +16,7 @@ class Overlay {
   }
 
   public highlightTargets(cells: Coordinates[]) {
-    this.board.highlightCells(cells, this.ENEMY_HIGHLIGHT_COLOR);
+    this.board.highlightCells(cells, this.TARGET_HIGHLIGHT_COLOR);
   }
 }
 
