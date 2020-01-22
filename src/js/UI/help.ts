79ac64ce9bce7d24d8e82253f48494414e3a39b2
Paul <paul@paulgoesdeep.com>
2020-01-22 19:11:22 +0300

    Update dollarsigns

--- src/js/UI/help.ts
+++ src/js/UI/help.ts
@@ -1,5 +1,5 @@
 import { AsyncRenderer, State } from "../lib/dom/async-renderer";
-import { DOM } from "dollarsigns";
+import * as DOM from "dollarsigns";
 
 class HelpDialog {
   private element: HTMLElement;
